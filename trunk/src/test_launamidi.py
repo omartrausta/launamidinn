da6776b14e8554e967f267b9a1b268df160cfd43
davidhalldor <davidhalldor@c436b4ae-8ff5-11de-800e-5b313e0d2bf3>
2009-08-23 16:15:06 +0000

    Testkodi virkar

--- trunk/src/test_launamidi.py
+++ trunk/src/test_launamidi.py
@@ -1,13 +1,12 @@
 # encoding: utf-8
 import unittest, launamidi
 
-class ProductTestCase(unittest.TestCase):
-    stimlanir = [("17.�g�st 2009",("09:00","10:00"))]
-    taxtar = [("dv",("09:00","17:00"),[0,1,2,3,4])]
-    nidurstada = [("dv",1.0)]
-    
+class ProductTestCase(unittest.TestCase):    
+    _stimplanir = [("17.�g�st 2009",("09:00","10:00"))]
+    _taxtar = [("dv",("09:00","17:00"),[0,1,2,3,4])]
+    _nidurstada = [("dv",1.0)]
     def testVinna(self):
-        utkoma = launamidi.vinna(stimplanir, taxtar)
-        self.failUnless(utkoma == nidurstada, 'Utreikningar fellu')
+        utkoma = launamidi.vinna(self._stimplanir, self._taxtar)
+        self.failUnless(utkoma == self._nidurstada, 'Utreikningar fellu')
 
 if __name__ == '__main__': unittest.main()
