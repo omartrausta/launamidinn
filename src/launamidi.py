251fa7b9019876b5c347422e06c64de591b7d0d1
davidhalldor <davidhalldor@c436b4ae-8ff5-11de-800e-5b313e0d2bf3>
2009-08-24 01:48:44 +0000

    Bætti við testum og bætti við smá regexp filteringum, nú er hægt að höndla inn og útstimplun

--- src/launamidi.py
+++ src/launamidi.py
@@ -19,13 +19,26 @@ _manudir = {'janúar':1,
 def manudurSemNumer(manudur):
     return _manudir.get(manudur)
 
-def regexp(s):
+def dateFilter(s):
     pattern = re.compile(r'[. ]')
     manudur = pattern.split(s)
-    return manudur[1]
+    return manudur
+
+def timeFilter(s):
+    pattern = re.compile(r'[:]')
+    timi = pattern.split(s)
+    return timi
 
 def vinna(stimplanir, taxtar):  
     for stimplun in stimplanir:
-        print regexp(stimplun[0])
-        print manudurSemNumer(regexp(stimplun[0]))
+        arrDagsetning = dateFilter(stimplun[0])
+        arrTimi = stimplun[1]
+        timiInn = timeFilter(arrTimi[0])
+        timiUt = timeFilter(arrTimi[1])
+        manNumer = manudurSemNumer(arrDagsetning[1])
+        inn = datetime.datetime(int(arrDagsetning[2]), manNumer ,int(arrDagsetning[0]), int(timiInn[0]), int(timiInn[1]))
+        ut = datetime.datetime(int(arrDagsetning[2]), manNumer ,int(arrDagsetning[0]), int(timiUt[0]), int(timiUt[1]))
+        print inn
+        print ut
+        print ut - inn
     return [("dv",1.0)]
--- src/test_launamidi.py
+++ src/test_launamidi.py
@@ -1,12 +1,22 @@
 # encoding: utf-8
 import unittest, launamidi
 
-class ProductTestCase(unittest.TestCase):    
+class LaunamidarTestCase(unittest.TestCase):    
     stimplanir = [("17.ágúst 2009",("09:00","10:00"))]
     taxtar = [("dv",("09:00","17:00"),[0,1,2,3,4])]
     nidurstada = [("dv",1.0)]
+    arrDagsetning = ['17', 'ágúst', '2009']
+       
     def testVinna(self):
         utkoma = launamidi.vinna(self.stimplanir, self.taxtar)
-        self.failUnless(utkoma == self.nidurstada, 'Utreikningar fellu')
+        self.failUnless(utkoma == self.nidurstada, 'Útreikningarnir féllu')
+    
+    def testRegexp(self):
+        utkoma = launamidi.dateFilter("17.ágúst 2009")
+        self.assertEqual(self.arrDagsetning, utkoma, 'Reglulega segðin féll')
+        
+    def testManudurSemNumer(self):
+        utkoma = launamidi.manudurSemNumer("ágúst")
+        self.assertEqual(8, utkoma, 'Fall sem skilar númer mánuðs féll')
 
 if __name__ == '__main__': unittest.main()
