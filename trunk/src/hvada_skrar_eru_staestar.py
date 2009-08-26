cc6189dad9506b9edd1958bfb5b2411e73bf87bb
gunnlaugursig@gmail.com <gunnlaugursig@gmail.com@c436b4ae-8ff5-11de-800e-5b313e0d2bf3>
2009-08-26 23:04:45 +0000

    úhú.....virkar :)

--- trunk/src/hvada_skrar_eru_staestar.py
+++ trunk/src/hvada_skrar_eru_staestar.py
@@ -2,39 +2,25 @@
 import os, sys, operator
 
 _listOfFiles=[]
-_numberOfFiles = 3
 
 def getlocaldata(sms,dr,flst):
-    idx = 1
-    
     for f in flst:
         fullf = os.path.join(dr,f)
-        if os.path.islink(fullf): 
-            continue
-        if os.path.isfile(fullf):
-            if len(_listOfFiles) == _numberOfFiles: # taka minnsta stakid
-                for i in _listOfFiles:
-                    print i[1]
-                    print os.path.getsize(fullf)
-                    print "--"
-                    if i[1] < os.path.getsize(fullf):
-                        _listOfFiles.remove(i)
-                        tup = (fullf, os.path.getsize(fullf))
-            else:
-                tup = (fullf, os.path.getsize(fullf))
-                _listOfFiles.append(tup)
-                _listOfFiles.sort()
+        if os.path.isfile(fullf): 
+            tup = (fullf, os.path.getsize(fullf))
+            _listOfFiles.append(tup)
+    tmp = sorted(_listOfFiles, key=operator.itemgetter(1))
+    sms[1] = tmp[operator.neg(sms[0]):] 
                 
 def dtstat(dtroot, number):
     _numberOfFiles = number
-    sums = []
+    sums = [number,0]
     os.path.walk(dtroot,getlocaldata,sums)
-
-def main():
-#    print "ello"
+    return sums[1]
     
-    report = dtstat('/home/gunnl/py/', 3)
-    print _listOfFiles
+def main():
+    root, numbers = sys.argv[1:]
+    report = dtstat(root, int(numbers))
+    print report
 
-if __name__== '__main__':
-    main()
+if __name__== "__main__": main()
