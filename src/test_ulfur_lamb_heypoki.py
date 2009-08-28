4f8f144db8bfdbb0b70d79e0306dbbb9faa8a30b
davidhalldor <davidhalldor@c436b4ae-8ff5-11de-800e-5b313e0d2bf3>
2009-08-28 18:18:19 +0000

    Lagaði unittestin og aðeins ulfur_lamb_heypoki svo það skili True í stað None objecti

--- src/test_ulfur_lamb_heypoki.py
+++ src/test_ulfur_lamb_heypoki.py
@@ -2,13 +2,11 @@
 import unittest, ulfur_lamb_heypoki
 
 class UlfurLambHeypokiTestCase(unittest.TestCase):
+    upphafsstada = ["bulh", ""]
+    expectedStada = True
+    
     def testVinna(self):
-        vandi = ["bulh", ""]
-        print vandi
-        print ulfur_lamb_heypoki.logleg_stada(vandi)
-        vandi = ulfur_lamb_heypoki.haegri(vandi, "u")
-        vandi = ulfur_lamb_heypoki.haegri(vandi, "b")
-        print vandi
-        print ulfur_lamb_heypoki.logleg_stada(vandi)
+        utkoma = ulfur_lamb_heypoki.keyra(self.upphafsstada)
+        self.assertEqual(self.expectedStada, utkoma)
 
 if __name__ == '__main__': unittest.main()
